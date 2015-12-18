29d8ace439f8c13fbca2a2f7cda8ed71172a9d67
deadlydragon00 <elmarrhex.gomez@gmail.com>
2015-12-18 21:57:10 +0800

    re-layout the FAB

--- fablayout/src/main/java/com/elmargomez/fablayout/widget/FabLayout.java
+++ fablayout/src/main/java/com/elmargomez/fablayout/widget/FabLayout.java
@@ -21,10 +21,13 @@ import android.content.Context;
 import android.os.Build;
 import android.support.design.widget.FloatingActionButton;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class FabLayout extends ViewGroup {
+
+    private static final int MAX_FAB_COUNT = 6;
 
     private boolean alignButtonRight = true;
     private boolean hasTittle = true;
@@ -53,30 +56,49 @@ public class FabLayout extends ViewGroup {
 
         int viewWidth = 0;
         int viewHeight = 0;
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(widthMeasureSpec);
 
         for (int x = 0; x < childSize; x++) {
             View view = getChildAt(x);
             boolean instanceOfView = view instanceof FloatingActionButton;
             if (instanceOfView && view.getVisibility() != GONE) {
-                if (MeasureSpec.getMode(widthMeasureSpec) == MeasureSpec.EXACTLY) {
-
-                } else if (MeasureSpec.getMode(widthMeasureSpec) == MeasureSpec.AT_MOST) {
-
-                } else {
-
-                }
+                measureChild(view, widthMeasureSpec, heightMeasureSpec);
+                viewWidth = Math.max(view.getMeasuredWidth(), viewWidth);
+                viewHeight += view.getMeasuredHeight();
+                Log.e("OnMeasure", ">>>> width: " + viewWidth + " height: " + viewHeight);
             } else if (!instanceOfView) {
+                // we want to remove the non FAB view
                 view.setVisibility(GONE);
             }
         }
 
+        switch (widthMode) {
+            case MeasureSpec.EXACTLY:
+                viewWidth = MeasureSpec.getSize(widthMeasureSpec);
+                break;
+        }
 
+        switch (heightMode) {
+            case MeasureSpec.EXACTLY:
+                viewHeight = MeasureSpec.getSize(heightMeasureSpec);
+                break;
+        }
 
         setMeasuredDimension(viewWidth, viewHeight);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-
+        final int childSize = getChildCount();
+        for (int x = 0; x < childSize; x++) {
+            View view = getChildAt(x);
+            boolean instanceOfView = view instanceof FloatingActionButton;
+            if (instanceOfView && view.getVisibility() != GONE) {
+                view.layout(r - view.getMeasuredWidth(), b - view.getMeasuredHeight(), r, b);
+                b -= view.getMeasuredHeight();
+            }
+        }
     }
+
 }
