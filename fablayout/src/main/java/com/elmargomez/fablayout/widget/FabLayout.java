c28d8d43608770429e978c9bacfeeb2dae428468
deadlydragon00 <elmarrhex.gomez@gmail.com>
2015-12-19 21:19:47 +0800

    added Layout Param

--- fablayout/src/main/java/com/elmargomez/fablayout/widget/FabLayout.java
+++ fablayout/src/main/java/com/elmargomez/fablayout/widget/FabLayout.java
@@ -17,6 +17,7 @@
 package com.elmargomez.fablayout.widget;
 
 import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.content.Context;
 import android.os.Build;
 import android.support.design.widget.FloatingActionButton;
@@ -24,6 +25,8 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class FabLayout extends ViewGroup {
 
@@ -54,19 +57,20 @@ public class FabLayout extends ViewGroup {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         final int childSize = getChildCount();
 
-        int viewWidth = 0;
-        int viewHeight = 0;
+        int occupiedWidth = 0;
+        int occupiedHeight = 0;
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-        int heightMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
 
         for (int x = 0; x < childSize; x++) {
             View view = getChildAt(x);
             boolean instanceOfView = view instanceof FloatingActionButton;
             if (instanceOfView && view.getVisibility() != GONE) {
-                measureChild(view, widthMeasureSpec, heightMeasureSpec);
-                viewWidth = Math.max(view.getMeasuredWidth(), viewWidth);
-                viewHeight += view.getMeasuredHeight();
-                Log.e("OnMeasure", ">>>> width: " + viewWidth + " height: " + viewHeight);
+                measureChildWithMargins(view, widthMeasureSpec, 0, heightMeasureSpec, 0);
+                LayoutParams layout = (LayoutParams) view.getLayoutParams();
+                int currentViewWidth = view.getMeasuredWidth() + layout.leftMargin + layout.rightMargin;
+                occupiedWidth = Math.max(currentViewWidth, occupiedWidth);
+                occupiedHeight += view.getMeasuredHeight() + layout.topMargin + layout.bottomMargin;
             } else if (!instanceOfView) {
                 // we want to remove the non FAB view
                 view.setVisibility(GONE);
@@ -75,29 +79,74 @@ public class FabLayout extends ViewGroup {
 
         switch (widthMode) {
             case MeasureSpec.EXACTLY:
-                viewWidth = MeasureSpec.getSize(widthMeasureSpec);
+                occupiedWidth = MeasureSpec.getSize(widthMeasureSpec);
                 break;
         }
 
         switch (heightMode) {
             case MeasureSpec.EXACTLY:
-                viewHeight = MeasureSpec.getSize(heightMeasureSpec);
+                occupiedHeight = MeasureSpec.getSize(heightMeasureSpec);
                 break;
         }
 
-        setMeasuredDimension(viewWidth, viewHeight);
+        setMeasuredDimension(occupiedWidth, occupiedHeight);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        Log.e("OnLayout",l+" "+t+" "+r+" "+b);
         final int childSize = getChildCount();
         for (int x = 0; x < childSize; x++) {
             View view = getChildAt(x);
             boolean instanceOfView = view instanceof FloatingActionButton;
             if (instanceOfView && view.getVisibility() != GONE) {
-                view.layout(r - view.getMeasuredWidth(), b - view.getMeasuredHeight(), r, b);
-                b -= view.getMeasuredHeight();
+                LayoutParams layoutParams = (LayoutParams) view.getLayoutParams();
+                int xCoordinate = (r - view.getMeasuredWidth()) - layoutParams.leftMargin;
+                int yCoordinate = (b - view.getMeasuredHeight()) - layoutParams.bottomMargin;
+                int width = r - layoutParams.rightMargin;
+                int height = b - layoutParams.topMargin;
+                view.layout(xCoordinate, yCoordinate, width, height);
+                b -= view.getMeasuredHeight() + layoutParams.topMargin + layoutParams.bottomMargin;
             }
+        }
+    }
+
+    @Override
+    protected ViewGroup.LayoutParams generateDefaultLayoutParams() {
+        return new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+    }
+
+    @Override
+    protected ViewGroup.LayoutParams generateLayoutParams(ViewGroup.LayoutParams p) {
+        return new LayoutParams(p);
+    }
+
+    @Override
+    public ViewGroup.LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new LayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected boolean checkLayoutParams(ViewGroup.LayoutParams p) {
+        return p instanceof LayoutParams;
+    }
+
+    public static class LayoutParams extends MarginLayoutParams {
+
+        public LayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+        }
+
+        public LayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        public LayoutParams(MarginLayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(ViewGroup.LayoutParams source) {
+            super(source);
         }
     }
 
